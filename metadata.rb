c283dd4502c1a7042fa3f7d158c48d6e46dfdc4c
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-04-29 10:11:48 +0000

    Namespace change to rackspace_kibana

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,15 @@
-name             'rackspace_kibana'
-maintainer       'Rackspace'
+name 'rackspace_kibana'
+maintainer 'Rackspace'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
-license          'Apache 2.0'
-description      'Installs and configures kibana'
+license 'Apache 2.0'
+description 'Installs and configures kibana'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version '1.0.0'
 
 %w(ubuntu debian redhat centos).each do |os|
   supports os
 end
 
-%w{git nginx apache2}.each do |cb|
+%w(git nginx apache2).each do |cb|
   depends cb
 end
